bd9f9bfaf8b4b3f41cfad84b920d67b1be1dd3d8
G1djet <gadjet.mot@mail.ru>
2024-06-14 23:12:13 +0700

    1

---
+++ imp.js
@@ -0,0 +1,17 @@
+// function getRandomPosition() {
+//     const x = Math.floor(Math.random() * 101) + '%';
+//     const y = Math.floor(Math.random() * 101) + '%';
+//     return { x, y };
+// }
+
+// function updateBackgroundPosition() {
+//     const { x, y } = getRandomPosition();
+//     document.body.style.setProperty('--x-pos', x);
+//     document.body.style.setProperty('--y-pos', y);
+// }
+
+// // Обновлять позицию фона каждые 10 секунд
+// setInterval(updateBackgroundPosition, 5000);
+
+// // Сразу установить начальную позицию
+// updateBackgroundPosition();
