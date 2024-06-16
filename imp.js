7a04d5f2395b225e200f71554c880dc28bce683a
G1djet <gadjet.mot@mail.ru>
2024-06-16 14:16:28 +0700

    1

--- imp.js
+++ imp.js
@@ -1,20 +1,4 @@
-// function getRandomPosition() {
-//     const x = Math.floor(Math.random() * 101) + '%';
-//     const y = Math.floor(Math.random() * 101) + '%';
-//     return { x, y };
-// }
 
-// function updateBackgroundPosition() {
-//     const { x, y } = getRandomPosition();
-//     document.body.style.setProperty('--x-pos', x);
-//     document.body.style.setProperty('--y-pos', y);
-// }
-
-// // Обновлять позицию фона каждые 10 секунд
-// setInterval(updateBackgroundPosition, 5000);
-
-// // Сразу установить начальную позицию
-// updateBackgroundPosition();
 ~function () {
     var video = document.querySelector('video')
   
@@ -28,3 +12,10 @@
     handleResize()
     window.addEventListener('resize', handleResize)
   }()
+$(document).foundation();
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
