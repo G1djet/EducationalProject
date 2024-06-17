525be5f7cb1f1c92f8f60b8025d9181566619f86
G1djet <gadjet.mot@mail.ru>
2024-06-17 13:53:18 +0700

    1

--- koncerty.js
+++ koncerty.js
@@ -1,26 +1,3 @@
-
-~function () {
-    var video = document.querySelector('video')
-  
-    function handleResize() {
-      var w0 = 320, h0 = 240, r0 = w0 / h0
-      var w = window.innerWidth, h = window.innerHeight, r = w / h
-      
-      video.style.transform = r0 < r ? 'scaleX(' + r / r0 + ')' : 'scaleY(' + r0 / r + ')'
-    }
-  
-    handleResize()
-    window.addEventListener('resize', handleResize)
-  }()
-$(document).foundation();
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  e.target.elements.name.value = '';
-  e.target.elements.email.value = '';
-  e.target.elements.message.value = '';
-});
-
-
 const concertTitles = document.querySelectorAll('.concert-title');
 const photoGalleries = document.querySelectorAll('.photo-gallery');
 const modal = document.querySelector('.modal');
