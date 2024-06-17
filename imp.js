e53d00fcbaf506c8c87c250eb21f5861c47c7637
G1djet <gadjet.mot@mail.ru>
2024-06-18 04:39:49 +0700

    1

--- imp.js
+++ imp.js
@@ -12,43 +12,5 @@
     handleResize()
     window.addEventListener('resize', handleResize)
   }()
-$(document).foundation();
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  e.target.elements.name.value = '';
-  e.target.elements.email.value = '';
-  e.target.elements.message.value = '';
-});
 
 
-const concertTitles = document.querySelectorAll('.concert-title');
-const photoGalleries = document.querySelectorAll('.photo-gallery');
-const modal = document.querySelector('.modal');
-const modalImage = document.querySelector('.modal-content img');
-const closeButton = document.querySelector('.close-button');
-
-concertTitles.forEach((title, index) => {
-  title.addEventListener('click', () => {
-    photoGalleries[index].style.display = photoGalleries[index].style.display === 'none' ? 'flex' : 'none';
-  });
-});
-
-photoGalleries.forEach((gallery) => {
-  const photos = gallery.querySelectorAll('img');
-  photos.forEach((photo) => {
-    photo.addEventListener('click', () => {
-      modalImage.src = photo.src;
-      modal.style.display = 'block';
-    });
-  });
-});
-
-closeButton.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
-
-window.addEventListener('click', (event) => {
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
-});
