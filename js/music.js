f5200e1e8962d30c1f039a5cd0b602274da30644
G1djet <gadjet.mot@mail.ru>
2024-06-18 14:19:29 +0700

    1

---
+++ js/music.js
@@ -0,0 +1,20 @@
+const albumCovers = document.querySelectorAll('.album-cover');
+const popup = document.querySelector('.popup');
+
+albumCovers.forEach(cover => {
+  cover.addEventListener('click', () => {
+    const backCover = cover.querySelector('.back');
+    backCover.style.transform = 'translateX(0)';
+    popup.style.display = 'block';
+  });
+});
+
+document.addEventListener('click', (event) => {
+  if (!event.target.closest('.album-cover')) {
+    const backCovers = document.querySelectorAll('.back');
+    backCovers.forEach(back => {
+      back.style.transform = 'translateX(100%)';
+    });
+    popup.style.display = 'none';
+  }
+});
